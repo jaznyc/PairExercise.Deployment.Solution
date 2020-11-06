a9c13493bebc51c8913494ffe4a8b68c5a06f826
Jasmine Hatcher <jasminehatchernyc@gmail.com>
2020-11-06 18:22:09 -0500

    Test

--- client/components/UserList.jsx
+++ client/components/UserList.jsx
@@ -5,6 +5,7 @@ export default ({ users }) => (
   <div className="user-list">
 
     <p className='lead'>Here's a list of {users.length} completely fictitious people:</p>
+    <p>This is a Test Line</p>
 
     <table className="table">
       <tbody>
